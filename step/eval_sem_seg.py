ceaa1a419c9370b71d3c71b307abc8d35b6eab1f
suhyunyoon <billy555@naver.com>
2021-07-20 16:51:02 +0900

    Add evaluating sem_seg Cityscapes dataset

--- step/eval_sem_seg.py
+++ step/eval_sem_seg.py
@@ -3,14 +3,23 @@ import numpy as np
 import os
 from chainercv.datasets import VOCSemanticSegmentationDataset
 from chainercv.evaluations import calc_semantic_segmentation_confusion
+
+#from torchvision.datasets import Cityscapes
+from chainercv.datasets import CityscapesSemanticSegmentationDataset
+
 import imageio
 
 def run(args):
-    dataset = VOCSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.voc12_root)
+    if args.dataset == 'voc12':
+        dataset = VOCSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.voc12_root)
+        ids = dataset.ids
+    elif args.dataset == 'cityscapes':
+        dataset = CityscapesSemanticSegmentationDataset(split=args.chainer_eval_set, data_dir=args.cityscapes_root, label_resolution=args.cityscapes_mode)
+        ids = list(map(lambda p: os.path.splitext(os.path.basename(p))[0], dataset.img_paths))
     labels = [dataset.get_example_by_keys(i, (1,))[0] for i in range(len(dataset))]
-
+    
     preds = []
-    for id in dataset.ids:
+    for id in ids:
         cls_labels = imageio.imread(os.path.join(args.sem_seg_out_dir, id + '.png')).astype(np.uint8)
         cls_labels[cls_labels == 255] = 0
         preds.append(cls_labels.copy())
